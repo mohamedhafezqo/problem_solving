84c83927b3de413590b69c27ad6dd7ed5b131ead
Mohamed Hafez <mohamed.hafezqo@gmail.com>
2020-04-10 00:04:46 +0200

    leetcode maximum-subarray solved

---
+++ Arrays/leetcode-maximum-subarray.js
@@ -0,0 +1,36 @@
+/**
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * status: easy
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * link: https://leetcode.com/problems/maximum-subarray/
+ */
+ 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSum = nums[0];
+    let i = 0;
+    
+    if (nums.length == 1) {
+        return maxSum;
+    }
+    
+    while (i < nums.length) {
+        let tempSum = nums[i] + nums[i-1];
+
+        if (nums[i] < tempSum) {
+            nums[i] = nums[i-1] + nums[i];
+        } else if (nums[i] > nums[i-1] && nums[i] < tempSum) {
+            nums[i] = nums[i-1] + nums[i];
+        }
+
+        maxSum = Math.max(maxSum, nums[i]);
+        
+        i++;
+    }
+    
+    return maxSum;
+};
